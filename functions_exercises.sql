79acb851ef34b8a24ec5da33fdc754959f0b394e
Larry Holmes <larry.b.holmes.2@gmail.com>
2022-07-06 14:27:41 -0500

    updating functions_exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -194,5 +194,5 @@ where to_date = '9999-01-01';
 -- A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the 
 -- employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. 
 SELECT LOWER (CONCAT (SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS username
-FROM employees
+FROM employees;
 
